a0a88946c6a89fdd8a43f1d3f449b9ec011b72c8
Justin Giancola <justin.giancola@gmail.com>
2014-09-18 15:58:04 +0000

    Prevent using module names incompatible with ember-cli naming conventions

--- config/initializers/es6_module_transpiler.rb
+++ config/initializers/es6_module_transpiler.rb
@@ -1,1 +1,15 @@
-ES6ModuleTranspiler.add_prefix_pattern Regexp.new(File.join(Rails.root, 'app/assets/javascripts')), 'es6-spike'
+prefix = 'es6-spike'
+
+ES6ModuleTranspiler.add_prefix_pattern Regexp.new(File.join(Rails.root, 'app/assets/javascripts')), prefix
+
+ES6ModuleTranspiler.transform = lambda do |name|
+  name_parts = name.downcase.sub("#{prefix}/", '').split(/[^a-z]/)
+  has_suffix = name_parts.length > 1 && name_parts.first.chomp('s') == name_parts.last
+  bad_characters = name.match(/[^a-z\d\/\-]/)
+
+  if bad_characters or has_suffix
+    raise NameError.new %Q[Module name "#{name}" is incompatible with ember-cli naming conventions]
+  end
+
+  name
+end
